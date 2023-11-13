e97d7219f97a000d7fbbbd29b10f31abb38f90a2
Roxyvick <141335682+Roxyvick@users.noreply.github.com>
2023-11-13 12:20:22 -0300

    Add files via upload

---
+++ Atividade2.java
@@ -0,0 +1,8 @@
+public class Atividade2 {
+    public static void main(String[] args) {
+        
+        String name ="Vitória";
+        String sobrenome =" Goulart";
+        System.out.println(name + sobrenome );
+    }
+}
